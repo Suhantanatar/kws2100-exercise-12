65bbfa077cfaf49b76e0ded8389424fbb269a622
BAS1C010 <sb.tanatar@gmail.com>
2026-04-13 15:06:28 +0200

    for api/grunnskole i nettside

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
 
 const app = new Hono();
-app.get("/api/grunnskole", (c) => c.json({ grunnskoler: true }));
+
+const db = new pg.Pool({
+  connectionString: "psql://postgres:postgres@localhost",
+});
+app.get("/api/grunnskole", async (c) => {
+  const result = await db.query("select * from spatial_ref_sys");
+  return c.json(result.rows);
+});
 // `serveStatic` makes Hono serve the output from `vite build`
 app.get("*", serveStatic({ root: "../dist" }));
 serve(app);
